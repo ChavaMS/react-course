d3676bd2e98ccb5443e21f61cd01e56c8a095cd5
ChavaMS <chava678ics@gmail.com>
2023-12-28 18:03:47 -0600

    API configured

--- 11-calendar-backend/index.js
+++ 11-calendar-backend/index.js
@@ -1,11 +1,20 @@
 const express = require('express');
+const { dbConnection } = require('./database/config.js');
+const cors = require('cors');
 require('dotenv').config();
 
 const app = express();
 
+// DB
+dbConnection();
+
+app.use(cors());
 
 app.use(express.static('public'));
+app.use(express.json());
 
+// Routes
+app.use('/api/auth', require('./routes/auth'));
 
 app.listen(process.env.PORT, () => {
     console.log(`Server running on port ${process.env.PORT}`);
---
+++ 11-calendar-backend/routes/auth.js
@@ -0,0 +1,24 @@
+const { Router } = require('express');
+const { createUser, loginUser, renewUser } = require('../controllers/auth');
+const { check } = require('express-validator');
+const { validarCampos } = require('../middlewares/validar-campos');
+const { validarJWT } = require('../middlewares/validar-jwt');
+const router = Router();
+
+
+router.post('/', [
+    check('email', 'El email es obligatorio').isEmail(),
+    check('password', 'El password debe de ser de 6 caracteres').isLength({ min: 6 }),
+    validarCampos
+], loginUser);
+router.post('/new', [
+    check('name', 'El nombre es obligatorio').not().isEmpty(),
+    check('email', 'El email es obligatorio').isEmail(),
+    check('password', 'El password debe de ser de 6 caracteres').isLength({ min: 6 }),
+    validarCampos
+], createUser);
+router.get('/renew', validarJWT, renewUser);
+
+
+
+module.exports = router;
